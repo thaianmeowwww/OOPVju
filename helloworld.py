81ad90bd847db7e0bf6eb65013656b61099c40dc
Thái An <thaian@gmail.com>
2026-03-05 15:35:50 +0700

    Thay doi

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,1 @@
-print('Xin chao the gioi')
-class HelloWorld:
-    def __init__(self, name):
-        self.name = name
-
-    def say_hello(self):
-        print(f'Xin chao {self.name}')
+print('Hello world!')
