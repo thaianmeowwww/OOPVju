d1c5afb1d76b5cac6c583c922cd15d68ffaaf6b3
Thái An <thaian@gmail.com>
2026-03-07 17:24:22 +0700

    Description

--- ThinkPY2.py
+++ ThinkPY2.py
@@ -31,3 +31,13 @@ str_minutes = str(minutes).zfill(2)
 str_seconds = str(seconds).zfill(2)
 
 print("Thời gian hiện tại (giờ GMT): " + str_hours + ":" + str_minutes + ":" + str_seconds)
+
+'''
+Cách tính thời gian hiện tại dựa trên số giây đã trôi qua kể từ 1/1/1970:
+1. Tính số ngày trôi qua bằng cách chia tổng số giây cho số giây trong một ngày (24*60*60).
+2. Tính số giây còn lại trong ngày hiện tại bằng cách lấy phần dư của tổng số giây chia cho số giây trong một ngày.
+3. Tính số giờ trôi qua trong ngày hiện tại bằng cách chia số giây còn lại cho số giây trong một giờ (60*60).
+4. Tính số giây còn lại trong giờ hiện tại bằng cách lấy phần dư của số giây còn lại chia cho số giây trong một giờ.
+5. Tính số phút trôi qua trong giờ hiện tại bằng cách chia số giây còn lại cho số giây trong một phút (60).
+6. Tính số giây trôi qua trong phút hiện tại bằng cách lấy phần dư của số giây còn lại chia cho số giây trong một phút.
+'''
