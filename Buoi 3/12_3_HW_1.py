059dba87bbd5a9cddba3557f88a0bae6a6ea18dd
Thái An <thaian@gmail.com>
2026-03-15 20:49:12 +0700

    BT buoi 3

--- Buoi 3/12_3_HW_1.py
+++ Buoi 3/12_3_HW_1.py
@@ -22,4 +22,41 @@ print(concho2)
 print(concho2.vayduoi())
 
 class Car:
-    def __init__ 
+    def __init__(self, brand, size, color, price):
+        self.brand = brand
+        self.size = size
+        self.color = color
+        self.price = price
+    def __str__(self):
+        return f"Xe {self.brand} có kích thước {self.size}, màu {self.color}, giá {self.price}." 
+    def speedup(self):
+        return f"{self.brand} đang tăng tốc."
+    def brake(self):
+        return f"{self.brand} đang phanh."
+    def dam(self):
+        return f"{self.brand} đang đâm vào vật cản."
+car1 = Car("Toyota", "Lớn", "Đỏ", 50000000)
+print(car1)
+print(car1.speedup())
+print(car1.brake())
+print(car1.dam())
+
+class Account:
+ def __init__(self, account_number, name, brand, balance):
+        self.account_number = account_number
+        self.name = name
+        self.brand = brand
+        self.balance = balance
+def __str__(self):
+        return f"Tài khoản {self.account_number} thuộc về {self.name}, ngân hàng {self.brand}, số dư {self.balance}."
+def rut_tien(self, amount):
+        if amount > self.balance:
+            return "Số dư không đủ để rút tiền."
+        else:
+            self.balance -= amount # Số dư = Số dư - Số tiền rút( -= = Số dư - Số tiền rút)
+            return f"Bạn đã rút {amount}. Số dư còn lại: {self.balance}."   
+def gui_tien(self, amount):
+        self.balance += amount # Số dư = Số dư + Số tiền gửi( += = Số dư + Số tiền gửi)
+        return f"Bạn đã gửi {amount}. Số dư còn lại: {self.balance}."       
+def check_balance(self):
+        return f"Số dư hiện tại của bạn là: {self.balance}."
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# OOPVju
-
-
+Buổi 1: Làm quen với OOP, BT: Hello world
+Buổi 2: Python căn bản với các BT tính toán, vẽ hình
+Buổi 3: BT OOP sơ khai
