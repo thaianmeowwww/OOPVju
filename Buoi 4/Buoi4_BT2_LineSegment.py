be33526db0ef032d40ebede8324960c9dd94365b
Thái An <thaian@gmail.com>
2026-03-26 12:57:31 +0700

    Chinh lai

--- Buoi 4/Buoi4_BT2_LineSegment.py
+++ Buoi 4/Buoi4_BT2_LineSegment.py
@@ -3,10 +3,8 @@ class Point:
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
-        
     def __str__(self):
         return f'({self.x}, {self.y})'
-
 class LineSegment:
     def __init__(self, *args):
         if len(args) == 0:
